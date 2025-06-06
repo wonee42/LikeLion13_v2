894941db4dcb6e67148845a68c0b5cccb4fd91c1
wonee42 <wonee422005@skuniv.ac.kr>
2025-06-06 15:14:41 +0900

    7주차 과제

---
+++ week07/week07/src/data/products.js
@@ -0,0 +1,72 @@
+export const products = [
+  {
+    id: 1,
+    name: "WHITE T-SHIRT",
+    price: 89050,
+    image: "https://storeau.taylorswift.com/cdn/shop/files/TS_AUTourWhiteT-Shirt-FRONT.png?v=1709617358&width=1100",
+    category: "TOP"
+  },
+  {
+    id: 2,
+    name: "HEART HANDS TANK",
+    price: 95900,
+    image: "https://storeau.taylorswift.com/cdn/shop/files/8tnC7rc2yP4qNiyzd6VwrmUhMEgM7wAk-3.png?v=1724071486&width=550",
+    category: "TOP"
+  },
+  {
+    id: 3,
+    name: "TAUPE T-SHIRT",
+    price: 95352,
+    image: "https://storeau.taylorswift.com/cdn/shop/files/TaylorSwift_TheErasTourTaupeT-Shirt_1.png?v=1715286958&width=550",
+    category: "TOP"
+  },
+  {
+    id: 4,
+    name: "GREY QUARTER ZIP",
+    price: 164400,
+    image: "https://storeau.taylorswift.com/cdn/shop/files/1-4f6riAEzxaVDnpy83Ccgf5KfIj9GOWxa_fa01e6d2-77da-45f5-9f9c-ed0406060a68.png?v=1742227424&width=1100",
+    category: "TOP"
+  },
+  {
+    id: 5,
+    name: "THE ERAS TOUR TAPESTRY",
+    price: 75350,
+    image: "https://storeau.taylorswift.com/cdn/shop/files/1-ALmTKMboXJ8Tkn6nosZPZECmhhG6bb2K.png?v=1742229594&width=1100",
+    category: "ACCESSORY"
+  },
+  {
+    id: 6,
+    name: "YOU NEED TO CALM DOWN HOODIE",
+    price: 178100,
+    image: "https://storeau.taylorswift.com/cdn/shop/files/PHOTO1_c6d9c227-4137-400d-a65d-3c49728b9ba3.png?v=1731507875&width=550",
+    category: "TOP"
+  },
+  {
+    id: 7,
+    name: "BEJEWLLED NECKLACE",
+    price: 75350,
+    image: "https://storeau.taylorswift.com/cdn/shop/files/Taylor_Swift_Bejeweled_Necklace_1_6afa85ea-1764-4182-8e24-0b9578198e0e.png?v=1731508070&width=550",
+    category: "ACCESSORY"
+  },
+  {
+    id: 8,
+    name: "AIN'T THAT THE WORST THING YOU EVER HEARD?",
+    price: 65760,
+    image: "https://storeau.taylorswift.com/cdn/shop/products/SWIFT_CRUELTEE_BACK.png?v=1699506598&width=550",
+    category: "TOP"
+  },
+  {
+    id: 9,
+    name: "1989 PINK JOGGER SWEATPANTS",
+    price: 157550,
+    image: "https://storeau.taylorswift.com/cdn/shop/files/1989_Taylor_sVersion_PinkJoggerSweatpants_d30a2b83-7cc2-4177-b5fb-04af67119251.png?v=1696491721&width=550",
+    category: "BOTTOM"
+  },
+  {
+    id: 10,
+    name: "1989 IS IT OVER NOW? ZIP UP HOODIE",
+    price: 178100,
+    image: "https://storeau.taylorswift.com/cdn/shop/files/PHOTO1_311ee8e7-eb85-43f3-87f7-8694160e6317.png?v=1731507835&width=550",
+    category: "TOP"
+  }
+];
